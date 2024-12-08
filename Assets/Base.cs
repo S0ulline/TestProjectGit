23fb3140e2e2cb088dfb31584f0c2b5f06bed0cf
Александр <mihaylovalex02@icloud.com>
2024-12-08 17:56:45 +0500

    first commit

---
+++ Assets/Base.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntity : MonoBehaviour
+{
+    public int hp = 100;
+    public int damage = 15;
+}
